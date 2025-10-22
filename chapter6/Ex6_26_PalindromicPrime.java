1d836c69a6610381805f02a8e2a7d9733f2c77a2
ytanii <ysft0729@gmail.com>
2025-10-22 02:45:10 -0400

    add Ex6_26_PalindromicPrime exercise

--- chapter6/Ex6_26_PalindromicPrime.java
+++ chapter6/Ex6_26_PalindromicPrime.java
@@ -2,9 +2,41 @@ import java.util.Scanner;
 
 public class Ex6_26_PalindromicPrime {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // Your code here
-        
+        int numberOfNumbers = 0;
+        for (int i = 2; numberOfNumbers < 100; i++) {
+            if (isPrime(i) && isPalindrome(i)) {
+                System.out.printf(i + " ");
+                numberOfNumbers++;
+                if (numberOfNumbers % 10 == 0) {
+                    System.out.println();
+                }
+            }
+
+        }
+
+
+    }
+
+    public static boolean isPrime(int number) {
+        for (int divisor = 2; divisor <= Math.sqrt(number); divisor++) {
+            if (number % divisor == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome(int number) {
+        int reversedNumber = 0;
+        int originalNumber = number;
+        while (number > 0) {
+            reversedNumber *= 10;
+            reversedNumber += number % 10;
+            number /= 10;
+        }
+
+        return reversedNumber == originalNumber;
+
+
     }
 }
