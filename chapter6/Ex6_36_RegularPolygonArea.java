4c1a58a3e8a2c0015e8032a91f66866e66b110ea
ytanii <ysft0729@gmail.com>
2025-10-22 16:23:07 -0400

    add Ex6_36_RegularPolygonArea exercise

--- chapter6/Ex6_36_RegularPolygonArea.java
+++ chapter6/Ex6_36_RegularPolygonArea.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 public class Ex6_36_RegularPolygonArea {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Your code here
-        
+
+        System.out.print("Enter the number of sides: ");
+        int numberOfSides = scanner.nextInt();
+        System.out.print("Enter the side: ");
+        double sideLength = scanner.nextDouble();
+        System.out.println("The area of the polygon is " + area(numberOfSides, sideLength));
+    }
+
+    public static double area(int numberOfSides, double side) {
+        return (numberOfSides * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / numberOfSides));
     }
 }
