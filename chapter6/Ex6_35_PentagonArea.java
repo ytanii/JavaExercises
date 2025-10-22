219ebec3ed65bc2f1d64f7becb810ae1e0b97685
ytanii <ysft0729@gmail.com>
2025-10-22 16:19:01 -0400

    add Ex6_35_PentagonArea exercise

--- chapter6/Ex6_35_PentagonArea.java
+++ chapter6/Ex6_35_PentagonArea.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class Ex6_35_PentagonArea {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Your code here
-        
+
+        System.out.print("Enter the side: ");
+        System.out.println("The area of the pentagon is " + area(scanner.nextDouble()));
+
+    }
+
+    public static double area(double side) {
+        return (5 * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / 5));
     }
 }
