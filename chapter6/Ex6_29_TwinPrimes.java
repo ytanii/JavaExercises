7f2a27a5aa59e861679884f1d07311a35fadcb11
ytanii <ysft0729@gmail.com>
2025-10-22 12:22:23 -0400

    add Ex6_29_TwinPrimes exercise

--- chapter6/Ex6_29_TwinPrimes.java
+++ chapter6/Ex6_29_TwinPrimes.java
@@ -2,9 +2,23 @@ import java.util.Scanner;
 
 public class Ex6_29_TwinPrimes {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // Your code here
-        
+
+
+        for (int i = 2; i <= 1000; i++) {
+            if (isPrime(i) && isPrime(i + 2)) {
+                System.out.println("(" + i + ", " + (i + 2) + ")");
+            }
+        }
+
+    }
+
+    public static boolean isPrime(int number) {
+        for (int divisor = 2; divisor <= Math.sqrt(number); divisor++) {
+            if (number % divisor == 0) {
+                return false;
+            }
+        }
+        return true;
+
     }
 }
