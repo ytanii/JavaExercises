1e0cee23b4ad06b135e5c9b3d11588d6cbad0d6f
ytanii <ysft0729@gmail.com>
2025-10-22 02:26:30 -0400

    add Ex6_25_ConvertMillis exercise

--- chapter6/Ex6_25_ConvertMillis.java
+++ chapter6/Ex6_25_ConvertMillis.java
@@ -3,8 +3,25 @@ import java.util.Scanner;
 public class Ex6_25_ConvertMillis {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Your code here
-        
+        System.out.print("Enter milliseconds: ");
+        long milliSeconds = scanner.nextLong();
+
+        System.out.println(milliSeconds + " is " + convertMillis(milliSeconds));
+
+
+    }
+
+    public static String convertMillis(long millis) {
+        long totalSeconds = millis / 1000;
+        long currentSecond = totalSeconds % 60;
+
+        long totalMinutes = totalSeconds / 60;
+        long currentMinute = totalMinutes % 60;
+
+        long totalHours = totalMinutes / 60;
+
+
+        return totalHours + ":" + currentMinute + ":" + currentSecond;
+
     }
 }
