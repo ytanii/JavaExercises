5bdb938d07eee7483330f13e35619eaf67d8f00e
ytanii <ysft0729@gmail.com>
2025-10-22 16:30:13 -0400

    add Ex6_37_FormatInteger exercise

--- chapter6/Ex6_37_FormatInteger.java
+++ chapter6/Ex6_37_FormatInteger.java
@@ -3,8 +3,35 @@ import java.util.Scanner;
 public class Ex6_37_FormatInteger {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Your code here
-        
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        System.out.print("Enter a width: ");
+        int width = scanner.nextInt();
+
+        System.out.println("The formated number is " + format(number, width));
+    }
+
+    public static String format(int number, int width) {
+        String leadingZeros = "";
+        if (getSize(number) > width) {
+            return "" + number;
+        } else {
+            for (int i = 0; i < width - getSize(number); i++) {
+                leadingZeros += "0";
+            }
+            return leadingZeros + number;
+        }
+
+    }
+
+    public static int getSize(int number) {
+        int count = 0;
+        while (number > 0) {
+            count++;
+            number /= 10;
+        }
+
+        return count;
     }
 }
