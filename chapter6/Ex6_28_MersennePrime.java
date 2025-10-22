b5215c19c8093fae1c7931af718dc4c73a7a3271
ytanii <ysft0729@gmail.com>
2025-10-22 12:11:19 -0400

    add Ex6_28_MersennePrime exercise

--- chapter6/Ex6_28_MersennePrime.java
+++ chapter6/Ex6_28_MersennePrime.java
@@ -2,9 +2,32 @@ import java.util.Scanner;
 
 public class Ex6_28_MersennePrime {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // Your code here
+
+        System.out.println("  p         2^p-1");
+        System.out.println("----------------------");
+        for(int p = 2; p <= 31; p++){
+            if(isPrime(p) && isMersennePrime(p)){
+                System.out.printf("%3d    %10d\n",p, ((int)(Math.pow(2,p) - 1)));
+            }
+        }
+
+    }
+
+    public static boolean isPrime(int number){
+        for(int divisor = 2; divisor <= Math.sqrt(number); divisor++){
+            if(number % divisor == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isMersennePrime(int number){
+        int x = (int) (Math.pow(2,number) - 1);
+        if(isPrime(x)){
+            return true;
+        }
+        return false;
         
     }
 }
