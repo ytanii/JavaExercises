5ac6a8d849a8b02bcab3a25312e436f9d2747b2d
ytanii <ysft0729@gmail.com>
2025-10-22 01:56:50 -0400

    add create-java.sh script for generating java files with boilerplate

---
+++ create-java.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "Usage: ./create-java.sh ClassName"
+    exit 1
+fi
+CLASS_NAME=$1
+FILE_NAME="${CLASS_NAME}.java"
+
+cat > "$FILE_NAME" << EOF
+import java.util.Scanner;
+
+public class $CLASS_NAME {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
+EOF
+
+echo "Created $FILE_NAME with class $CLASS_NAME"
