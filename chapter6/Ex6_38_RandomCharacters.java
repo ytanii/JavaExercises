ff815418e25c2794b93da43179aed28ada65e03b
ytanii <ysft0729@gmail.com>
2025-10-22 16:38:44 -0400

    add Ex6_38_RandomCharacters exercise

--- chapter6/Ex6_38_RandomCharacters.java
+++ chapter6/Ex6_38_RandomCharacters.java
@@ -3,8 +3,33 @@ import java.util.Scanner;
 public class Ex6_38_RandomCharacters {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Your code here
-        
+
+        for(int i = 1; i <= 100; i++){
+            printRandomUppercaseLetter();
+            if(i % 50 == 0){
+                System.out.println();
+            }
+        }
+        for(int i = 1; i <= 100; i++){
+            printRandomDigit();
+            if(i % 50 == 0){
+                System.out.println();
+            }
+        }
+
+
     }
+
+    public static void printRandomUppercaseLetter(){
+        System.out.print(generateRandomCharacter('A','Z'));
+    }
+
+    public static void printRandomDigit(){
+        System.out.print(generateRandomCharacter('0','9'));
+    }
+
+    public static char generateRandomCharacter(char ch1, char ch2){
+        return (char)((Math.random()*(ch2 - ch1))+ch1);
+    }
+
 }
