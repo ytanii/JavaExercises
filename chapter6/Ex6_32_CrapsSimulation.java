5408c4cd00c9267c0d654aab12ace8611705b39f
ytanii <ysft0729@gmail.com>
2025-10-22 14:38:34 -0400

    add Ex6_32_CrapsSimulation exercise

--- chapter6/Ex6_32_CrapsSimulation.java
+++ chapter6/Ex6_32_CrapsSimulation.java
@@ -1,10 +1,43 @@
 import java.util.Scanner;
 
 public class Ex6_32_CrapsSimulation {
+
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // Your code here
-        
+        int wins = 0;
+        for (int i = 0; i < 10000; i++) {
+            if (isWinner()) {
+                wins++;
+            }
+
+        }
+        System.out.println(wins);
+
+    }
+
+    public static int resultOfRoll() {
+        int roll1 = (int) (Math.random() * 6) + 1;
+        int roll2 = (int) (Math.random() * 6) + 1;
+        return roll1 + roll2;
+    }
+
+    public static boolean isWinner() {
+        int rollNumber = resultOfRoll();
+        if (rollNumber == 7 || rollNumber == 11) {
+
+            return true;
+        } else if (rollNumber == 2 || rollNumber == 3 || rollNumber == 12) {
+
+            return false;
+        } else {
+            int point = rollNumber;
+            int newRollNumber = resultOfRoll();
+            while (newRollNumber != 7 && newRollNumber != point) {
+                newRollNumber = resultOfRoll();
+            }
+            return newRollNumber == point;
+        }
+
+
     }
 }
