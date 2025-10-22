79e08bb45486fa549e081e86f9f712b9e12d314f
ytanii <ysft0729@gmail.com>
2025-10-22 03:04:07 -0400

    add Ex6_27_Emirp exercise

--- chapter6/Ex6_27_Emirp.java
+++ chapter6/Ex6_27_Emirp.java
@@ -2,9 +2,43 @@ import java.util.Scanner;
 
 public class Ex6_27_Emirp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // Your code here
-        
+        int numberOfNumbers = 0;
+        for (int i = 2; numberOfNumbers < 100; i++) {
+            if (isEmirp(i)) {
+                System.out.print(i + " ");
+                numberOfNumbers++;
+                if (numberOfNumbers % 10 == 0) {
+                    System.out.println();
+                }
+            }
+        }
+
+
+    }
+
+
+    public static boolean isEmirp(int number) {
+        return (isPrime(number) && isPrime(reverse(number))) && !(number == reverse(number));
+
+
+    }
+
+    public static int reverse(int number) {
+        int reversedNumber = 0;
+        while (number > 0) {
+            reversedNumber *= 10;
+            reversedNumber += number % 10;
+            number /= 10;
+        }
+        return reversedNumber;
+    }
+
+    public static boolean isPrime(int number) {
+        for (int divisor = 2; divisor <= Math.sqrt(number); divisor++) {
+            if (number % divisor == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
