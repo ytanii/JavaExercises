dfa617f6e07f36e896a6e8c24ee5c2bc52f6d7e2
ytanii <ysft0729@gmail.com>
2025-10-23 18:14:25 -0400

    add Ex7_07_CountSingleDigits exercise

--- chapter7/Ex7_07_CountSingleDigits.java
+++ chapter7/Ex7_07_CountSingleDigits.java
@@ -4,5 +4,16 @@ public class Ex7_07_CountSingleDigits {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] counts = new int[10];
+
+        for(int i = 0; i < 100; i++) {
+            counts[(int) (Math.random() * 10)]++;
+        }
+
+
+        for(int i = 0; i < counts.length; i++){
+
+            System.out.println(i + " occurs " + counts[i] + (counts[i] > 1 ? " times" : " time"));
+        }
     }
 }
