04be3a0c1fe4c58f3131d9f21150ff1533fbf756
ytanii <ysft0729@gmail.com>
2025-10-23 15:03:51 -0400

    add Ex7_03_CountOccurrences exercise

--- chapter7/Ex7_03_CountOccurrences.java
+++ chapter7/Ex7_03_CountOccurrences.java
@@ -4,5 +4,27 @@ public class Ex7_03_CountOccurrences {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] counts = new int[101];
+
+        System.out.print("Enter the integers between 1 and 100: ");
+        for (int i = 0; i < counts.length; i++) {
+            int input = scanner.nextInt();
+            if (input == 0) {
+                break;
+            } else if (input > 100 || input < 0) {
+                System.out.println("Enter an integer between 1 and 100");
+                continue;
+            }
+            counts[input]++;
+        }
+
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] > 0) {
+                System.out.println(i + " occurs " + counts[i] + (counts[i] > 1 ? " times" : " time"));
+            }
+        }
+
+
     }
+
 }
