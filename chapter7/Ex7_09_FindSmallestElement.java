760ee15261bb7e717ac31ad11f1334d92d7fd317
ytanii <ysft0729@gmail.com>
2025-10-23 19:06:24 -0400

    add Ex7_09_FindSmallestElement exercise

--- chapter7/Ex7_09_FindSmallestElement.java
+++ chapter7/Ex7_09_FindSmallestElement.java
@@ -3,6 +3,23 @@ import java.util.Scanner;
 public class Ex7_09_FindSmallestElement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double[] numbers = new double[10];
 
+        System.out.print("Enter 10 doubles: ");
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = scanner.nextDouble();
+        }
+        System.out.println("The min is " + min(numbers));
+
+    }
+
+    public static double min(double[] array){
+        double min = array[0];
+        for(int i = 1; i < array.length; i++){
+            if(min > array[i]){
+                min = array[i];
+            }
+        }
+        return min;
     }
 }
