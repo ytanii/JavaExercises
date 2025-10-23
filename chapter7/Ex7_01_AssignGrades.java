9110f481446d3edd54c0e1dedeeec3ac13cd86e4
ytanii <ysft0729@gmail.com>
2025-10-23 14:10:15 -0400

    add Ex7_01_AssignGrades exercise

--- chapter7/Ex7_01_AssignGrades.java
+++ chapter7/Ex7_01_AssignGrades.java
@@ -4,5 +4,53 @@ public class Ex7_01_AssignGrades {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the number of students: ");
+        int size = scanner.nextInt();
+
+        int[] scores = new int[size];
+
+        System.out.print("Enter " + size + " scores: ");
+        for (int i = 0; i < scores.length; i++) {
+            scores[i] = scanner.nextInt();
+
+        }
+
+        displayGrades(scores);
+
+
+    }
+
+    public static int getMaxScore(int[] scores) {
+        int max = scores[0];
+        for (int i = 0; i < scores.length; i++) {
+            if (max < scores[i]) {
+                max = scores[i];
+            }
+        }
+        return max;
+    }
+
+    public static char getGrade(int[] scores, int studentNumber) {
+        int best = getMaxScore(scores);
+
+        if (scores[studentNumber] >= best - 10) {
+            return 'A';
+
+        } else if (scores[studentNumber] >= best - 20) {
+            return 'B';
+        } else if (scores[studentNumber] >= best - 30) {
+            return 'C';
+        } else if (scores[studentNumber] >= best - 40) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+
+    }
+
+    public static void displayGrades(int[] scores) {g
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println("Student " + i + " score is " + scores[i] + " and grade is " + getGrade(scores, i));
+        }
     }
 }
