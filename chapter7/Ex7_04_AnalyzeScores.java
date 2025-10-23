e2e88c8e0cb545638fed6777e7c95f2be2c9d23c
ytanii <ysft0729@gmail.com>
2025-10-23 15:17:41 -0400

    add Ex7_04_AnalyzeScores exercise

--- chapter7/Ex7_04_AnalyzeScores.java
+++ chapter7/Ex7_04_AnalyzeScores.java
@@ -4,5 +4,45 @@ public class Ex7_04_AnalyzeScores {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double[] scores = new double[100];
+        double sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < scores.length; i++) {
+            double input = scanner.nextDouble();
+            if (input < 0) {
+                break;
+            }
+            scores[i] = input;
+            sum += input;
+            count++;
+        }
+
+        double average = sum / count;
+        int countAboveAverage = 0;
+        int countBelowAverage = 0;
+        int countAverage = 0;
+
+        for (int i = 0; i < count; i++) {
+            if (scores[i] < average) {
+                countBelowAverage++;
+            } else if (scores[i] == average) {
+                countAverage++;
+
+            } else {
+                countAboveAverage++;
+            }
+
+        }
+
+        System.out.println("Number of scores above the average of " + average + " is " + countAboveAverage);
+        System.out.println("Number of scores below the average of " + average + " is " + countBelowAverage);
+        if (countAverage > 0) {
+            System.out.println("Number of scores equal to the average of " + average + " is " + countAverage);
+
+
+        }
+
+
     }
 }
