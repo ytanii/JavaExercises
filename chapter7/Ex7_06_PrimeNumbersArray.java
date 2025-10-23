04ed1696ebef672a20c1434d0a4826fe15a5fd8d
ytanii <ysft0729@gmail.com>
2025-10-23 16:30:09 -0400

    add Ex7_06_PrimeNumbersArray exercise

--- chapter7/Ex7_06_PrimeNumbersArray.java
+++ chapter7/Ex7_06_PrimeNumbersArray.java
@@ -1,8 +1,37 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ex7_06_PrimeNumbersArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] primes = new int[50];
+        primes[0] = 2;
+        int index = 0;
+        int k = 0;
+
+        for (int i = 3; primes[49] == 0; i++) {
+            boolean isPrime = true;
+
+            for(int j = 0; primes[j] <= Math.sqrt(i); j++) {
+                if (i % primes[j] == 0) {
+                    isPrime = false;
+                    break;
+
+                }
+            }
+
+            if(isPrime){
+                primes[k+1] = i;
+                k++;
+
+            }
+
+        }
+
+
+        for(int i = 0; i < primes.length; i++){
+            System.out.print(primes[i] + " ");
+        }
     }
 }
