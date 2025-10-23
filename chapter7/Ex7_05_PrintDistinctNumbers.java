3fa75d41e8bd43f09d2c31893b252254e105bd83
ytanii <ysft0729@gmail.com>
2025-10-23 15:58:43 -0400

    add Ex7_05_PrintDistinctNumbers exercise

--- chapter7/Ex7_05_PrintDistinctNumbers.java
+++ chapter7/Ex7_05_PrintDistinctNumbers.java
@@ -4,5 +4,38 @@ public class Ex7_05_PrintDistinctNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] numbers = new int[10];
+
+        System.out.print("Enter 10 numbers: ");
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = scanner.nextInt();
+        }
+
+        int[] distinctNumbers = new int[10];
+        int count = 0;
+
+        for(int i = 0; i < numbers.length; i++){
+            boolean isDistinct = true;
+            for(int j = 0; j < distinctNumbers.length; j++){
+                if(distinctNumbers[j] == numbers[i]){
+                    isDistinct = false;
+                    break;
+                }
+
+            }
+            if(isDistinct){
+                distinctNumbers[count] = numbers[i];
+                count++;
+            }
+
+        }
+
+        System.out.println("The number of distinct numbers is " + count);
+        System.out.print("The distinct numbers are: ");
+        for(int i = 0; i < count; i++){
+                System.out.print(distinctNumbers[i] + " ");
+            }
+        }
+
     }
-}
+
