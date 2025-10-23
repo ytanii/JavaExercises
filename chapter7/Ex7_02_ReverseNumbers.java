fda90051f290043ab2d4f89843efe345f1b0afb6
ytanii <ysft0729@gmail.com>
2025-10-23 14:41:19 -0400

    add Ex7_02_ReverseNumbers exercise

--- chapter7/Ex7_02_ReverseNumbers.java
+++ chapter7/Ex7_02_ReverseNumbers.java
@@ -4,5 +4,27 @@ public class Ex7_02_ReverseNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] numbers = new int[10];
+
+        System.out.print("Enter 10 integers: ");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        reverse(numbers);
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+
+    }
+
+    public static void reverse(int[] numbers) {
+        for (int i = numbers.length - 1, j = 0; j < numbers.length / 2; i--, j++) {
+            int helper = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = helper;
+        }
     }
 }
