bcee8b6d57692d3d44e10ce5e3c3a2ebba255402
ytanii <ysft0729@gmail.com>
2025-10-24 14:11:46 -0400

    add Ex7_23_LockerPuzzle exercise

--- chapter7/Ex7_23_LockerPuzzle.java
+++ chapter7/Ex7_23_LockerPuzzle.java
@@ -1,8 +1,35 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ex7_23_LockerPuzzle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        boolean[] lockerStatusList = new boolean[100];
+
+
+        for(int i = 1; i <= 100; i++){
+            if(i == 1){
+                for(int j = 0; j < lockerStatusList.length; j++){
+                    lockerStatusList[j] = !lockerStatusList[j];
+                }
+            }else{
+                for(int j = i - 1; j < lockerStatusList.length; j += i){
+                        lockerStatusList[j] = !lockerStatusList[j];
+
+                }
+
+            }
+
+        }
+
+
+        System.out.print("Open lockers: ");
+        for(int i = 0; i < lockerStatusList.length; i++){
+            if(lockerStatusList[i]){
+                System.out.print(i+1 + " ");
+            }
+        }
 
     }
 }
