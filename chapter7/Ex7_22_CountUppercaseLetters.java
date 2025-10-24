372ba4318c512523ee4a4c789cbe0dd16525b115
ytanii <ysft0729@gmail.com>
2025-10-24 13:48:10 -0400

    add Ex7_22_CountUppercaseLetters exercise

--- chapter7/Ex7_22_CountUppercaseLetters.java
+++ chapter7/Ex7_22_CountUppercaseLetters.java
@@ -4,5 +4,20 @@ public class Ex7_22_CountUppercaseLetters {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+        if(args.length == 0){
+            System.out.println("Enter a string");
+            System.exit(1);
+        }
+        String str = args[0];
+        int upperCaseLetterCount = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(Character.isUpperCase(str.charAt(i))){
+                upperCaseLetterCount++;
+            }
+        }
+
+        System.out.println("The number of uppercase letters in the string is " + upperCaseLetterCount);
+
     }
 }
