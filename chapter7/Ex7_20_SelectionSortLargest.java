422fc16dd07e5b0545c5719b00ae949229c6f971
ytanii <ysft0729@gmail.com>
2025-10-24 13:41:08 -0400

    add Ex7_20_SelectionSortLargest exercise

--- chapter7/Ex7_20_SelectionSortLargest.java
+++ chapter7/Ex7_20_SelectionSortLargest.java
@@ -1,8 +1,39 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ex7_20_SelectionSortLargest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double[] numbers = new double[10];
+        System.out.println("Enter 10 doubles: ");
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextDouble();
+        }
+
+        System.out.println();
+
+
+        selectionSort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+    public static void selectionSort(double[] numbers) {
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            int maxIndex = i;
+            for (int j = i - 1; j >= 0; j--) {
+                if (numbers[maxIndex] < numbers[j]) {
+                    maxIndex = j;
+                }
+            }
+
+            double helper = numbers[i];
+            numbers[i] = numbers[maxIndex];
+            numbers[maxIndex] = helper;
+
+        }
     }
 }
