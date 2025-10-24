1c18e3feff76f0592cde4687bae94299d8135881
ytanii <ysft0729@gmail.com>
2025-10-24 13:44:12 -0400

    add Ex7_21_SumIntegers exercise

--- chapter7/Ex7_21_SumIntegers.java
+++ chapter7/Ex7_21_SumIntegers.java
@@ -4,5 +4,13 @@ public class Ex7_21_SumIntegers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int totalSum = 0;
+        for (String number : args) {
+            totalSum += Integer.parseInt(number);
+
+        }
+
+        System.out.println("The total is " + totalSum);
+
     }
 }
