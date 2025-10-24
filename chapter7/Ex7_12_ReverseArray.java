e5826d2dbe9e3a27376fa5cd2a1528181c3dd48d
ytanii <ysft0729@gmail.com>
2025-10-23 20:29:31 -0400

    add Ex7_12_ReverseArray exercise

--- chapter7/Ex7_12_ReverseArray.java
+++ chapter7/Ex7_12_ReverseArray.java
@@ -1,8 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ex7_12_ReverseArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] numbers = new int[10];
+        System.out.print("Enter 10 numbers: ");
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = scanner.nextInt();
+        }
+
+        System.out.println(Arrays.toString(reverse(numbers)));
+    }
+
+    public static int[] reverse(int[] numbers){
+        for(int i = 0, j = numbers.length - 1; i < numbers.length / 2; i++,j--){
+            int helper = numbers[i];
+            numbers [i] = numbers[j];
+            numbers[j] = helper;
+        }
+        return numbers;
     }
 }
