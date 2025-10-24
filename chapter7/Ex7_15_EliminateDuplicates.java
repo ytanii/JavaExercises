f9863acf32ea54d7ed191bdf0255192ade2b02b1
ytanii <ysft0729@gmail.com>
2025-10-24 10:59:26 -0400

    add Ex7_15_EliminateDuplicates exercise

--- chapter7/Ex7_15_EliminateDuplicates.java
+++ chapter7/Ex7_15_EliminateDuplicates.java
@@ -3,6 +3,45 @@ import java.util.Scanner;
 public class Ex7_15_EliminateDuplicates {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[] list = new int[10];
+
+        System.out.print("Enter 10 numbers: ");
+        for (int i = 0; i < list.length; i++) {
+            list[i] = scanner.nextInt();
+        }
+
+        int[] noDuplicatesList = eliminateDuplicates(list);
+        System.out.print("The distinct numbers are: ");
+        for (int i = 0; i < noDuplicatesList.length; i++) {
+            System.out.print(noDuplicatesList[i] + " ");
+        }
+    }
+
+    public static int[] eliminateDuplicates(int[] list) {
+
+        int[] nonDuplicateList = new int[list.length];
+        int count = 0;
+        for (int i = 0; i < list.length; i++) {
+            boolean isDuplicate = false;
+            for (int j = 0; j < count; j++) {
+                if (list[i] == nonDuplicateList[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                nonDuplicateList[count] = list[i];
+                count++;
+            }
+        }
+
+        int[] result = new int[count];
+        for (int i = 0; i < count; i++) {
+            result[i] = nonDuplicateList[i];
+        }
+        return result;
 
     }
+
+
 }
