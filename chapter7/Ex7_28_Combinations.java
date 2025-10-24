f0174b7b985dbca029d76aa727a193dd16771e2b
ytanii <ysft0729@gmail.com>
2025-10-24 19:10:20 -0400

    add Ex7_28_Combinations exercise

--- chapter7/Ex7_28_Combinations.java
+++ chapter7/Ex7_28_Combinations.java
@@ -4,5 +4,21 @@ public class Ex7_28_Combinations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] numbers = new int[10];
+        System.out.print("Enter 10 integers: ");
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        int combinationCount = 0;
+        for (int i = 0; i < numbers.length; i++) {
+
+            for (int j = 1 + i; j < numbers.length; j++) {
+                System.out.println(numbers[i] + " " + numbers[j]);
+            }
+
+        }
     }
+
 }
