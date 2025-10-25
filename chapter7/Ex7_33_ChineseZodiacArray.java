4cd0581887e9705a4d14be22b5572c745d111bca
ytanii <ysft0729@gmail.com>
2025-10-25 00:47:31 -0400

    add Ex7_33_ChineseZodiacArray exercise

--- chapter7/Ex7_33_ChineseZodiacArray.java
+++ chapter7/Ex7_33_ChineseZodiacArray.java
@@ -4,5 +4,19 @@ public class Ex7_33_ChineseZodiacArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        System.out.println(getYearAnimal(year));
+
+    }
+
+    public static String getYearAnimal(int year) {
+        int index = year % 12;
+
+        String[] animals = {"monkey", "rooster", "dog", "pig", "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "sheep"};
+
+        return animals[index];
+
     }
 }
