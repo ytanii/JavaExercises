4f13c2d40a3d5d1f107b3f5e3a4690efb928c6a7
ytanii <ysft0729@gmail.com>
2025-10-24 20:22:18 -0400

    add Ex7_30_IsConsecutiveFour exercise

--- chapter7/Ex7_30_IsConsecutiveFour.java
+++ chapter7/Ex7_30_IsConsecutiveFour.java
@@ -4,5 +4,35 @@ public class Ex7_30_IsConsecutiveFour {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the number of values: ");
+        int[] numbers = new int[scanner.nextInt()];
+
+        System.out.print("Enter the values: ");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        
+        System.out.println((isConsecutiveFour(numbers) ? "The list has consecutive fours" : "The list has no consecutive fours"));
+
+    }
+
+    public static boolean isConsecutiveFour(int[] numbers) {
+        int count = 0;
+
+        for (int i = 1; i <= numbers.length - 1; i++) {
+
+            if (numbers[i] == numbers[i - 1]) {
+                count++;
+                if (count == 3) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+
+        }
+        return false;
+
+
     }
 }
