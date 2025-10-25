37a2c6ed52625823fea64751b105566aece9595d
ytanii <ysft0729@gmail.com>
2025-10-25 11:48:58 -0400

    add Ex7_34_SortCharacters exercise

--- chapter7/Ex7_34_SortCharacters.java
+++ chapter7/Ex7_34_SortCharacters.java
@@ -4,5 +4,40 @@ public class Ex7_34_SortCharacters {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+
+
+        System.out.println(sort(str));
+
+
+    }
+
+    public static String sort(String s) {
+        char[] chars = new char[s.length()];
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = s.charAt(i);
+        }
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < s.length(); j++) {
+                if (chars[minIndex] > chars[j]) {
+                    minIndex = j;
+                }
+            }
+
+            int helper = chars[i];
+            chars[i] = chars[minIndex];
+            chars[minIndex] = (char) helper;
+
+        }
+        String newString = "";
+
+        for (char x : chars) {
+            newString += x;
+        }
+
+        return newString;
     }
 }
