b518c72abdd08016d6fb3f1ab5fe351d054ab7d7
ytanii <ysft0729@gmail.com>
2025-10-26 13:34:43 -0400

    add Ex8_02_SumMajorDiagonal exercise

--- chapter8/Ex8_02_SumMajorDiagonal.java
+++ chapter8/Ex8_02_SumMajorDiagonal.java
@@ -4,5 +4,34 @@ public class Ex8_02_SumMajorDiagonal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double[][] matrix = readMatrix();
+
+        System.out.println("Sum of the elements in the major diagonal is " + sumMajorDiagonal(matrix));
+
+    }
+
+    public static double[][] readMatrix() {
+        Scanner scanner = new Scanner(System.in);
+
+        double[][] matrix = new double[4][4];
+
+        System.out.println("Enter a 4-by-4 matrix row by row");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = scanner.nextDouble();
+            }
+        }
+
+        return matrix;
+
+    }
+
+    public static double sumMajorDiagonal(double[][] matrix) {
+        double sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            sum += matrix[i][i];
+
+        }
+        return sum;
     }
 }
