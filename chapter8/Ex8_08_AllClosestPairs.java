26578e7bd4e44ea10e6dae9b1a0e42bb40be345a
ytanii <ysft0729@gmail.com>
2025-10-27 00:21:02 -0400

    add Ex8_08_AllClosestPairs exercise

--- chapter8/Ex8_08_AllClosestPairs.java
+++ chapter8/Ex8_08_AllClosestPairs.java
@@ -3,6 +3,54 @@ import java.util.Scanner;
 public class Ex8_08_AllClosestPairs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of points: ");
+        int numberOfPoints = scanner.nextInt();
 
+        double[][] points = new double[numberOfPoints][2];
+
+        System.out.println("Enter " + numberOfPoints + " points: ");
+        for (int i = 0; i < points.length; i++) {
+            points[i][0] = scanner.nextDouble();
+            points[i][1] = scanner.nextDouble();
+        }
+
+        int p1 = 0, p2 = 1;
+        double shortestDistances = distance(points[p1][0], points[p1][1], points[p2][0], points[p2][1]);
+
+        int[][] indices = new int[points.length][2];
+        int k = 0;
+
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                double distance = distance(points[i][0], points[i][1], points[j][0], points[j][1]);
+
+                if (distance < shortestDistances) {
+                    k = 0;
+                    indices[k][0] = i;
+                    indices[k][1] = j;
+                    shortestDistances = distance;
+                } else if (distance == shortestDistances) {
+                    indices[k][0] = i;
+                    indices[k][1] = j;
+                    k++;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < k; i++) {
+
+            p1 = indices[i][0];
+            p2 = indices[i][1];
+
+            System.out.println("The closest two points are " + "(" + points[p1][0] + ", " + points[p1][1] + ") and (" + points[p2][0] + ", " + points[p2][1] + ")");
+        }
+
+        System.out.println("Their distance is " + shortestDistances);
+
+    }
+
+    public static double distance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 }
