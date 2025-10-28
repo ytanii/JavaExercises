c5738b54451151a727d459aaefcd477c806b647d
ytanii <ysft0729@gmail.com>
2025-10-28 12:50:12 -0400

    add Ex8_15_SameLine exercise

--- chapter8/Ex8_15_SameLine.java
+++ chapter8/Ex8_15_SameLine.java
@@ -4,5 +4,33 @@ public class Ex8_15_SameLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter five points: ");
+        double[][] points = readPoints();
+
+        if (sameLine(points)) {
+            System.out.println("The five points are on the same line ");
+        } else {
+            System.out.println("The five points are not on the same line");
+        }
+    }
+
+    public static double[][] readPoints() {
+        Scanner scanner = new Scanner(System.in);
+        double[][] points = new double[5][2];
+        for (int i = 0; i < points.length; i++) {
+            points[i][0] = scanner.nextDouble();
+            points[i][1] = scanner.nextDouble();
+        }
+
+        return points;
+    }
+
+    public static boolean sameLine(double[][] points) {
+        for (int i = 0; i < points.length; i++) {
+            if ((points[1][0] - points[0][0]) * (points[i][1] - points[0][1]) - (points[i][0] - points[0][0]) * (points[1][1] - points[0][1]) != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
