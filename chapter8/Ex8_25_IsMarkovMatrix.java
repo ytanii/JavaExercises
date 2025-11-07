bd180ae4bb1d385b1616a31e7ac26e80bf51b508
ytanii <ysft0729@gmail.com>
2025-11-07 17:29:43 -0500

    add Ex8_25_IsMarkovMatrix exercise

--- chapter8/Ex8_25_IsMarkovMatrix.java
+++ chapter8/Ex8_25_IsMarkovMatrix.java
@@ -2,7 +2,69 @@ import java.util.Scanner;
 
 public class Ex8_25_IsMarkovMatrix {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a 3-by-3 matrix row by row:");
+        double[][] matrix = getMatrix();
+
+
+        if (checkMarkovMatrix(matrix)) {
+            System.out.println("It is a Markov matrix");
+        } else {
+            System.out.println("It is not a Markov matrix");
+        }
+
+
+    }
+
+    public static double[][] getMatrix() {
+        Scanner scanner = new Scanner(System.in);
+        double[][] matrix = new double[3][3];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = scanner.nextDouble();
+            }
+        }
+
+        return matrix;
+    }
+
+    public static boolean checkMarkovMatrix(double[][] matrix) {
+        if (allPositive(matrix) && allColumnSumsOne(matrix)) {
+            return true;
+
+        } else {
+            return false;
+        }
+
+    }
+
+    public static double sumColumn(double[][] matrix, int i) {
+        double sum = 0;
+        for (int j = 0; j < matrix.length; j++) {
+            sum += matrix[j][i];
+        }
+        return sum;
+    }
+
+    public static boolean allPositive(double[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] < 0) {
+                    return false;
+
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean allColumnSumsOne(double[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            if (sumColumn(matrix, i) != 1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
