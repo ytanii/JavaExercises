8dc72b1b7989b4f6b185713dc372e44600f59305
ytanii <ysft0729@gmail.com>
2025-11-09 17:49:46 -0500

    add Ex8_29_IdenticalArrays and Ex8_30_LinearEquation exercises

--- chapter8/Ex8_29_IdenticalArrays.java
+++ chapter8/Ex8_29_IdenticalArrays.java
@@ -1,8 +1,41 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Ex8_29_IdenticalArrays {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter list1: ");
+        int[][] m1 = getMatrix();
+        System.out.print("Enter list2: ");
+        int[][] m2 = getMatrix();
 
+        if (equals(m1, m2))
+            System.out.println("The two arrays are identical");
+        else
+            System.out.println("The two arrays are not identical");
+    }
+
+    public static int[][] getMatrix() {
+        Scanner input = new Scanner(System.in);
+        int[][] m = new int[3][3];
+        for (int i = 0; i < m.length; i++)
+            for (int j = 0; j < m[i].length; j++)
+                m[i][j] = input.nextInt();
+        return m;
+    }
+
+    public static boolean equals(int[][] m1, int[][] m2) {
+        int[] a = new int[m1.length * m1[0].length];
+        int[] b = new int[m2.length * m2[0].length];
+        int k = 0;
+        for (int[] row : m1)
+            for (int n : row)
+                a[k++] = n;
+        k = 0;
+        for (int[] row : m2)
+            for (int n : row)
+                b[k++] = n;
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a, b);
     }
 }
--- chapter8/Ex8_30_LinearEquation.java
+++ chapter8/Ex8_30_LinearEquation.java
@@ -3,6 +3,42 @@ import java.util.Scanner;
 public class Ex8_30_LinearEquation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 2 linear equations to solve (for example ax + bx = c, enter a b c): ");
+        double[][] equations = new double[2][3];
 
+        for (int i = 0; i < equations.length; i++) {
+            for (int j = 0; j < equations[i].length; j++) {
+                equations[i][j] = scanner.nextDouble();
+
+            }
+        }
+
+        double[] b = new double[2];
+        b[0] = equations[0][2];
+        b[1] = equations[1][2];
+        double[][] a = new double[2][2];
+        a[0][0] = equations[0][0];
+        a[0][1] = equations[0][1];
+        a[1][0] = equations[1][0];
+        a[1][1] = equations[1][1];
+
+        if(linearEquation(a,b) != null) {
+            double[] solution = linearEquation(a, b);
+            System.out.println("x is " + solution[0]);
+            System.out.println("y is " + solution[1]);
+        }else{
+            System.out.println("The equation has no solution");
+        }
+
+    }
+
+    public static double[] linearEquation(double[][] a, double[] b) {
+        double denominator = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+        if (denominator == 0) {
+            return null;
+        }
+        double x = ((b[0] * a[1][1]) - (b[1] * a[0][1])) / denominator;
+        double y =((b[1] * a[0][0]) - (b[0] * a[1][0]))/ denominator;
+        return new double[]{x,y};
     }
 }
