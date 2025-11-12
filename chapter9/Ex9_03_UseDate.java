844285aa9660a9dc811e9cb1f4472e589df9384f
ytanii <154643447+ytanii@users.noreply.github.com>
2025-11-11 23:00:08 -0500

    add Ex9_03_UseDate exercise

--- chapter9/Ex9_03_UseDate.java
+++ chapter9/Ex9_03_UseDate.java
@@ -1,8 +1,44 @@
+import java.util.Date;
 import java.util.Scanner;
 
 public class Ex9_03_UseDate {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Date date = new Date();
+
+        date.setTime(10000);
+        System.out.println(date.toString());
+        System.out.println();
+
+        date.setTime(100000);
+        System.out.println(date.toString());
+        System.out.println();
+
+        date.setTime(1000000);
+        System.out.println(date.toString());
+        System.out.println();
+
+        date.setTime(10000000);
+        System.out.println(date.toString());
+        System.out.println();
+
+        date.setTime(100000000);
+        System.out.println(date.toString());
+        System.out.println();
+
+        date.setTime(1000000000);
+        System.out.println(date.toString());
+        System.out.println();
+
+        date.setTime(10000000000L);
+        System.out.println(date.toString());
+        System.out.println();
+
+        date.setTime(100000000000L);
+        System.out.println(date.toString());
+        System.out.println();
+
 
     }
 }
