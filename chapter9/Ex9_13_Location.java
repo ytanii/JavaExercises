d9eff8c385ac77d2c2c8746ee7a32dc562213830
ytanii <154643447+ytanii@users.noreply.github.com>
2025-11-12 13:38:54 -0500

    add Ex9_13_Location exercise and Location class with UML diagram

--- chapter9/Ex9_13_Location.java
+++ chapter9/Ex9_13_Location.java
@@ -4,5 +4,19 @@ public class Ex9_13_Location {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the number of rows and columns in the array: ");
+        double[][] matrix = new double[scanner.nextInt()][scanner.nextInt()];
+
+        System.out.println("Enter the array: ");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = scanner.nextDouble();
+            }
+        }
+
+        Location location = Location.locateLargest(matrix);
+
+        System.out.println("The location of the largest element is " + location.maxValue + " at " + "(" + location.row + ", " + location.column + ")");
+
     }
 }
---
+++ chapter9/Location.java
@@ -0,0 +1,31 @@
+public class Location {
+
+    public int row;
+    public int column;
+    public double maxValue;
+
+
+    public Location(int row, int column, double maxValue) {
+        this.row = row;
+        this.column = column;
+        this.maxValue = maxValue;
+
+    }
+
+    public static Location locateLargest(double[][] a) {
+        int maxIndexRow = 0;
+        int maxIndexColumn = 0;
+        double maxValue = a[0][0];
+        for (int i = 1; i < a.length; i++) {
+            for (int j = 1; j < a[i].length; j++) {
+                if (maxValue < a[i][j]) {
+                    maxValue = a[i][j];
+                    maxIndexRow = i;
+                    maxIndexColumn = j;
+                }
+            }
+        }
+
+        return new Location(maxIndexRow, maxIndexColumn,maxValue);
+    }
+}
