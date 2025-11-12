1f58d0fd5e60bc40b95701fbb8ce546884eec8a2
ytanii <154643447+ytanii@users.noreply.github.com>
2025-11-11 23:53:56 -0500

    add Ex9_05_UseGregorianCalendar  exercise

--- chapter9/Ex9_05_UseGregorianCalendar.java
+++ chapter9/Ex9_05_UseGregorianCalendar.java
@@ -1,8 +1,17 @@
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class Ex9_05_UseGregorianCalendar {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        GregorianCalendar calendar = new GregorianCalendar();
+
+        System.out.println(+calendar.get(GregorianCalendar.MONTH) + "/" + calendar.get(GregorianCalendar.DAY_OF_MONTH) + "/" + calendar.get(GregorianCalendar.YEAR));
+
+        System.out.println();
+
+        calendar.setTimeInMillis(1234567898765L);
+        System.out.println(+calendar.get(GregorianCalendar.MONTH) + "/" + calendar.get(GregorianCalendar.DAY_OF_MONTH) + "/" + calendar.get(GregorianCalendar.YEAR));
 
     }
 }
