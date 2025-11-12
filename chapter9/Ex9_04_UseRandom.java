e114c08f96c2ed9afa1e91ee57aff027ccaabb55
ytanii <154643447+ytanii@users.noreply.github.com>
2025-11-11 23:12:37 -0500

    add Ex9_04_UseRandom exercise

--- chapter9/Ex9_04_UseRandom.java
+++ chapter9/Ex9_04_UseRandom.java
@@ -1,8 +1,15 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ex9_04_UseRandom {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Random generator = new Random(1000);
+
+        for (int i = 0; i < 50; i++) {
+            System.out.println(generator.nextInt(100));
+        }
 
     }
 }
