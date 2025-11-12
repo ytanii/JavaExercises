924009a5cce420bdb4a820b75905063de67f6c1e
ytanii <154643447+ytanii@users.noreply.github.com>
2025-11-12 09:29:16 -0500

    add Ex9_07_Account exercise and Account class with UML diagram

--- chapter9/Ex9_07_Account.java
+++ chapter9/Ex9_07_Account.java
@@ -4,5 +4,20 @@ public class Ex9_07_Account {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Account account = new Account(1122, 20000);
+        Account.setAnnualInterestRate(4.5);
+
+        account.withdraw(2500);
+
+        account.deposit(3000);
+
+
+        System.out.printf(
+                "Account %d: balance = $%.2f, monthly interest = $%.2f, created on %s%n",
+                account.getId(),
+                account.getBalance(),
+                account.getMonthlyInterest(),
+                account.getDateCreated()
+        );
     }
 }
