989b10056405be7e185fb8bcc0521e12b2c1520c
ytanii <154643447+ytanii@users.noreply.github.com>
2025-11-11 19:52:31 -0500

    add Ex9_01_Rectangle exercise and Rectangle class with UML diagram

--- chapter9/Ex9_01_Rectangle.java
+++ chapter9/Ex9_01_Rectangle.java
@@ -4,5 +4,16 @@ public class Ex9_01_Rectangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Rectangle rectangle1 = new Rectangle(4,40);
+        Rectangle rectangle2 = new Rectangle(3.5,35.9);
+
+        System.out.println("rectangle1's width is " + rectangle1.getWidth() + " and it's height " + rectangle1.getHeight());
+        System.out.println("rectangle1's area is " + rectangle1.getArea() + " and it's perimeter " + rectangle1.getPerimeter());
+
+        System.out.println();
+
+        System.out.println("rectangle2's width is " + rectangle2.getWidth() + " and it's height " + rectangle2.getHeight());
+        System.out.println("rectangle2's area is " + rectangle2.getArea() + " and it's perimeter " + rectangle2.getPerimeter());
+
     }
 }
---
+++ chapter9/Rectangle.java
@@ -0,0 +1,30 @@
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width > 0 ? width : 1;
+        this.height = height > 0 ? height : 1;
+    }
+
+    public Rectangle() {
+        this(1, 1);
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (this.width + this.height);
+    }
+
+}
