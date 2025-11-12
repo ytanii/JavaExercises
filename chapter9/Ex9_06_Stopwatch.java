eb654766b867d7ea50c5e347a3a7dc7bdedd0cef
ytanii <154643447+ytanii@users.noreply.github.com>
2025-11-12 08:42:45 -0500

    add Ex9_06_Stopwatch exercise and Stopwatch class with UML diagram

--- chapter9/Ex9_06_Stopwatch.java
+++ chapter9/Ex9_06_Stopwatch.java
@@ -1,8 +1,45 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ex9_06_Stopwatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] array = createArray();
+
+        Stopwatch stopwatch = new Stopwatch();
+
+
+        stopwatch.start();
+        sort(array);
+        stopwatch.stop();
+
+        System.out.println("It took " + stopwatch.getEndTime() + " - " + stopwatch.getStartTime() + " = " + stopwatch.getElapsedTime() + " milliseconds to selection sort 100000 numbers");
+
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int indexOfMin = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[indexOfMin] > array[j]) {
+                    indexOfMin = j;
+                }
+            }
+            int helper = array[i];
+            array[i] = array[indexOfMin];
+            array[indexOfMin] = helper;
+        }
+
+    }
+
+    public static int[] createArray() {
+        int[] array = new int[100000];
+
+        for (int i = 0; i < 100000; i++) {
+            array[i] = (int) (Math.random() * 100001);
+        }
+
+        return array;
     }
 }
---
+++ chapter9/Stopwatch.java
@@ -0,0 +1,29 @@
+public class Stopwatch {
+    private long startTime;
+    private long endTime;
+
+    public Stopwatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public long getStartTime() {
+        return this.startTime;
+    }
+
+    public long getEndTime() {
+        return this.endTime;
+    }
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        this.endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return this.endTime - this.startTime;
+    }
+
+}
