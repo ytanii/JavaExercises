0f02d4afc62fe9c795192409d371bf955665beab
ytanii <154643447+ytanii@users.noreply.github.com>
2025-11-12 11:50:40 -0500

    add Ex9_10_QuadraticEquation exercise and QuadraticEquation class with UML diagram

--- chapter9/Ex9_10_QuadraticEquation.java
+++ chapter9/Ex9_10_QuadraticEquation.java
@@ -4,5 +4,20 @@ public class Ex9_10_QuadraticEquation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter values for a, b, c as a b c");
+        QuadraticEquation equation = new QuadraticEquation(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble());
+
+        double discriminant = equation.getDiscriminant();
+
+        if (discriminant >= 0) {
+            if (discriminant == 0) {
+                System.out.println("root: " + equation.getRoot1());
+            } else {
+                System.out.println("root1: " + equation.getRoot1() + ", root2: " + equation.getRoot2());
+            }
+        } else {
+            System.out.println("The equation has no roots.");
+        }
+
     }
 }
