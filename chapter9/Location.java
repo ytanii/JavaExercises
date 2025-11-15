dcb901136a34667a19861e15a18befa6ac32dad7
ytanii <154643447+ytanii@users.noreply.github.com>
2025-11-14 19:18:27 -0500

    update Location class

--- chapter9/Location.java
+++ chapter9/Location.java
@@ -16,8 +16,8 @@ public class Location {
         int maxIndexRow = 0;
         int maxIndexColumn = 0;
         double maxValue = a[0][0];
-        for (int i = 1; i < a.length; i++) {
-            for (int j = 1; j < a[i].length; j++) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
                 if (maxValue < a[i][j]) {
                     maxValue = a[i][j];
                     maxIndexRow = i;
