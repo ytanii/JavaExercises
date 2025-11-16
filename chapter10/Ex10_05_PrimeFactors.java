50805d4055980a038fa51e75a9ac67b11a6ff7ef
ytanii <154643447+ytanii@users.noreply.github.com>
2025-11-16 18:40:20 -0500

    update Ex10_05_PrimeFactors exercise

--- chapter10/Ex10_05_PrimeFactors.java
+++ chapter10/Ex10_05_PrimeFactors.java
@@ -7,46 +7,22 @@ public class Ex10_05_PrimeFactors {
         System.out.print("Enter a positive integer: ");
         int number = scanner.nextInt();
 
-        int[] factors= factor(number);
-        StackOfIntegers stack = new StackOfIntegers(factors.length);
+        StackOfIntegers stack = new StackOfIntegers();
 
-        for(int i  = 0; i < factors.length; i++){
-            stack.push(factors[i]);
-        }
-
-        while(!stack.empty()){
-            System.out.print(stack.pop());
-            if(!stack.empty()){
-                System.out.print(", ");
-            }
-        }
-        System.out.println();
-
-    }
-
-    public static int[] factor(int number) {
-        if(number == 1){
-            return new int[] {};
-        }
-
-        int count = 0;
-        int temp = number;
-        for(int i = 2; i <= temp; i++){
-            while(temp % i == 0){
-                count++;
-                temp /= i;
-            }
-        }
-
-        int[] factors = new int[count];
-        int index = 0;
-        for(int i = 2; i <= number; i++){
-            while(number % i == 0){
-                factors[index++] = i;
+        for (int i = 2; i <= number; i++) {
+            while (number % i == 0) {
+                stack.push(i);
                 number /= i;
             }
         }
+        while (!stack.empty()) {
+            int topNumber = stack.pop();
+            if (stack.empty()) {
+                System.out.print(topNumber);
+            } else {
+                System.out.print(topNumber + ", ");
+            }
+        }
 
-        return factors;
     }
 }
