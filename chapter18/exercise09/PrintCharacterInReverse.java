b6ee23d524cb1d2d8251941543986726a69cecd6
ytanii <ysft0729@gmail.com>
2026-03-18 12:33:23 -0400

    add chapter 18 exercise 9

---
+++ chapter18/exercise09/PrintCharacterInReverse.java
@@ -0,0 +1,27 @@
+package exercise09;
+
+import java.util.Scanner;
+
+public class PrintCharacterInReverse {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = scanner.nextLine();
+        reverseDisplay(str);
+
+    }
+
+    public static void reverseDisplay(String str) {
+        reverseDisplay(str, str.length() - 1);
+
+    }
+
+    public static void reverseDisplay(String str, int high) {
+        if (high < 0) {
+            return;
+        }
+        System.out.print(str.charAt(high));
+        reverseDisplay(str, high - 1);
+    }
+}
