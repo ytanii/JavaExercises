347ff9a856b2a35a0998e4dd2dc84b336cfcd1ff
ytanii <ysft0729@gmail.com>
2026-03-18 15:36:12 -0400

    add chapter 18 exercise 12

--- chapter18/exercise09/PrintStringInReverse.java
+++ chapter18/exercise09/PrintStringInReverse.java
@@ -2,7 +2,7 @@ package exercise09;
 
 import java.util.Scanner;
 
-public class PrintCharacterInReverse {
+public class PrintStringInReverse {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
